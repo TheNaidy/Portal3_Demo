444cbb462ddba4840ed009324c16ebd239e768c4
Alvaro Goncalves <Alvaro Goncalves>
2016-11-11 23:29:01 +0200

    Extended the license service class

---
+++ Portal3_prototype/src/app/models/assetsearch.model.ts
@@ -0,0 +1,8 @@
+export class AssetSearchModel{
+    
+    id:string;
+    name:string;
+    code:string;
+    status:string;
+    
+}
--- Portal3_prototype/src/app/services/license.service.ts
+++ Portal3_prototype/src/app/services/license.service.ts
@@ -1,8 +1,32 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/rx';
+import 'rxjs/add/operator/map';
+
+import { AssetSearchModel } from '../models/assetsearch.model';
+import { AssetModel } from '../models/asset.model';
 
 @Injectable()
 export class LicenseService {
 
-  constructor() { }
+  private baseURL: string = "http://[replace]/api";
+
+  constructor(public _http: Http) {
+
+  }
+
+  getLicensesBySearch(licenseSearchID: any): Observable<AssetSearchModel[]> {
+    let licensesearchURL = this.baseURL + '/licensesearch/' + licenseSearchID;
+
+    return this._http.get(licensesearchURL)
+      .map(res => res.json());
+  }
+
+  getLicense(licenseID: any): Observable<AssetModel> {
+    let licenseURL = this.baseURL + '/licenses/' + licenseID;
+
+    return this._http.get(licenseURL)
+      .map(res => res.json());
+  }
 
 }
