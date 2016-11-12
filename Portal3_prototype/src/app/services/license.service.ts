a863d948f1b0f32027ac768c27fc02e85bf5a2f0
Alvaro Goncalves <Alvaro Goncalves>
2016-11-12 16:12:37 +0200

    License Service update

--- Portal3_prototype/src/app/services/license.service.ts
+++ Portal3_prototype/src/app/services/license.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
 import { Observable } from 'rxjs/rx';
 import 'rxjs/add/operator/map';
 
@@ -10,22 +10,29 @@ import { AssetModel } from '../models/asset.model';
 export class LicenseService {
 
   private baseURL: string = "http://[replace]/api";
+  private headers: Headers;
+  private options: RequestOptions;
 
   constructor(public _http: Http) {
+    this.headers = new Headers();
+    this.headers.append('Content-Type', 'application/json');
 
+    this.options = new RequestOptions({
+      headers: this.headers
+    });
   }
 
   getLicensesBySearch(licenseSearchID: any): Observable<AssetSearchModel[]> {
     let licensesearchURL = this.baseURL + '/licensesearch/' + licenseSearchID;
 
-    return this._http.get(licensesearchURL)
+    return this._http.get(licensesearchURL, this.options)
       .map(res => res.json());
   }
 
   getLicense(licenseID: any): Observable<AssetModel> {
     let licenseURL = this.baseURL + '/licenses/' + licenseID;
 
-    return this._http.get(licenseURL)
+    return this._http.get(licenseURL, this.options)
       .map(res => res.json());
   }
 
