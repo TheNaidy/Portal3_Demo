8d76cab3a819bacda16b86560a99d5e945c4fb02
Toby-Mills <Mills.toby@gmail.com>
2016-11-18 14:45:28 +0200

    Added new filterable-list component

---
+++ Portal3_prototype/src/app/filterable-list/filterable-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filterable-list',
+  templateUrl: './filterable-list.component.html',
+  styleUrls: ['./filterable-list.component.css']
+})
+export class FilterableListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
