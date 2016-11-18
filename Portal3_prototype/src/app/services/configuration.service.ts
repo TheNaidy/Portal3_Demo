a766577e246438506e59c88bd3868e92d24f5062
Toby-Mills <Mills.toby@gmail.com>
2016-11-18 14:42:39 +0200

    Bug fix knock on from page-configuration rename

--- Portal3_prototype/src/app/services/configuration.service.ts
+++ Portal3_prototype/src/app/services/configuration.service.ts
@@ -1,23 +1,20 @@
 import { Injectable } from '@angular/core';
 import{ ListConfiguration } from '../list-configuration';
-import { ColumnConfiguration } from '../column-configuration'
-import { PageConfiguration } from '../shared/pageConfiguration'
+import { ColumnConfiguration } from '../column-configuration';
+import { PageConfiguration } from '../shared/page-configuration';
 
 @Injectable()
 export class ConfigurationService {
-  
-  //public listConfiguration: ListConfiguration;
-  //public savedSearch: any;
+  // public listConfiguration: ListConfiguration;
+  // public savedSearch: any;
   public pageConfiguration:PageConfiguration;
 
-  constructor() {  
+  constructor() { }
 
-   }
-
-   GetPageConfiguration(idPage:any):PageConfiguration {
-     //Dummy GUID
+   GetPageConfiguration(idPage: any): PageConfiguration {
+     // Dummy GUID
      this.pageConfiguration = new PageConfiguration();
-     this.pageConfiguration.SavedSearch='FCCD63CE-ECE5-4722-B720-AB23349CEFAC';
+     this.pageConfiguration.SavedSearch = 'FCCD63CE-ECE5-4722-B720-AB23349CEFAC';
 
      this.pageConfiguration.ListConfiguration = new ListConfiguration();
      this.pageConfiguration.ListConfiguration.columns = [];
