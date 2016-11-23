c883f80a31ede53be9e57d4b9a3f725546c4fb2f
Toby-Mills <Mills.toby@gmail.com>
2016-11-23 19:27:56 +0200

    Added date formatting support to the List component

--- Portal3_prototype/src/app/list/list-format.pipe.ts
+++ Portal3_prototype/src/app/list/list-format.pipe.ts
@@ -1,5 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { DataType } from '../models/attribute';
+import { environment } from '../../environments/environment';
 
 @Pipe({
   name: 'listFormat'
@@ -9,10 +10,14 @@ export class ListFormatPipe implements PipeTransform {
   transform(value: any, dataType: DataType): string {
     let formatted: string;
 
-   switch (dataType) {
+    switch (dataType) {
       case DataType.string: formatted = value;
         break;
-      case DataType.date: formatted = value.substring(0, 10);
+      case DataType.date: formatted = new Date(value).toLocaleDateString();
+        break;
+      case DataType.number: formatted = (+value).toLocaleString();
+        break;
+      case DataType.guid: formatted = value;
         break;
     }
     return formatted;
