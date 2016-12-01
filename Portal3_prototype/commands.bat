497294ff864149053a6de401090d6cce47c23234
Duncan Terblanche <duncan.terblanche@hotmail.com>
2016-12-01 10:42:26 +0200

    Fixed commands

--- Portal3_prototype/commands.bat
+++ Portal3_prototype/commands.bat
@@ -1,4 +1,4 @@
-npm install -g angular-cli@1.0.0-beta.20-4
-npm install -g typings@2.0.0
-typings install --global github:Esri/jsapi-resources/4.x/typescript/arcgis-js-api.d.ts
-npm install :: This command MUST be at the end of the list.
+call npm install
+call npm install -g angular-cli@1.0.0-beta.20-4
+call npm install -g typings@2.0.0
+call typings install --global github:Esri/jsapi-resources/4.x/typescript/arcgis-js-api.d.ts
